eead800c1160a367fbf6743ac6990e4326d251d0
aaryyya <aryampatil2005@gmail.com>
2025-09-10 16:15:26 +0530

    Data Tyes and Variables

---
+++ 01._Vars_Datatypes.py
@@ -0,0 +1,15 @@
+print("Arya","My Age 15" )
+print(23,45)
+print(4+5)
+name="shradha"
+name="meow"
+print(name)
+print("my name:",name) 
+age=int(input("Age: "))
+price=float(input("Price: "))
+name=input("Name: ")
+print("My name is", name,"I am ",age, "years old,","price for me is" ,price)
+
+
+
+
