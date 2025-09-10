ee7481cf7543cd5ae0f618e9a129fa3a093639ee
aaryyya <aryampatil2005@gmail.com>
2025-09-11 00:09:40 +0530

    Type Conversion and Csting

---
+++ 10._Quetions.py
@@ -0,0 +1,8 @@
+side=float(input("Enter Side here: "))
+areaofsq=side*side
+print(areaofsq)
+
+num1=float(input("Enter Number_1 for calculating average here: "))
+num2=float(input("Enter Number_2 for calculating average here: "))
+average=(num1+num2)/2
+print(average)
