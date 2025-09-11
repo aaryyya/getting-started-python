ff1c4309b12a0ba83bf6b1ceec8e11afb2ba03e4
aaryyya <aryampatil2005@gmail.com>
2025-09-11 12:48:47 +0530

    Questions Basic and Strings added

--- 02._Conditional_Statements.py
+++ 02._Conditional_Statements.py
@@ -1,51 +1,51 @@
 
-# age=12
-# if(age>=18):
-#     print("vote")
-# elif(age<18):
-#     print("cant")
-# else:
-#     print("okay")
+age=12
+if(age>=18):
+    print("vote")
+elif(age<18):
+    print("cant")
+else:
+    print("okay")
 
-# light=input("Enter Traffic Color:")
-# if(light=="red"):
-#     print("stop")
-# elif(light=="green"):
-#     print("go")
-# elif(light=="yellow"):
-#     print("watch and go")
-# else:
-#     print("invalid colour")
-
-
-# marks=float(input("Enter Marks here: "))
-# if(marks>=90):
-#     print("A")
-# elif(marks>=80 and marks<90):
-#     print("B")
-# elif(marks>=70 and marks<80):
-#     print("B")
-# else:
-#     print("D")
-#     print("Try Harder and dont fail")
+light=input("Enter Traffic Color:")
+if(light=="red"):
+    print("stop")
+elif(light=="green"):
+    print("go")
+elif(light=="yellow"):
+    print("watch and go")
+else:
+    print("invalid colour")
 
 
-# #Sinlge Line Conditional Statements
-# #var=val1 if(condition)else val2
-# food=input("food: ")
-# eat="Yes" if food =="cake" else "no"
-# print(eat)
+marks=float(input("Enter Marks here: "))
+if(marks>=90):
+    print("A")
+elif(marks>=80 and marks<90):
+    print("B")
+elif(marks>=70 and marks<80):
+    print("B")
+else:
+    print("D")
+    print("Try Harder and dont fail")
 
-# food=input("food: ")
-# print("sweet") if food== "cake" or food== "jalebi" else print("not sweet")
 
-# age =int(input("age:"))
-# vote=("yes","no")[age<=18] #first value is for when the statement is false and secong for if it is true
-# print(vote)
+#Sinlge Line Conditional Statements
+#var=val1 if(condition)else val2
+food=input("food: ")
+eat="Yes" if food =="cake" else "no"
+print(eat)
 
-# sal=float(input("salary:"))
-# tax=sal*(0.1,0.2)[sal<=5000]
-# print(tax)
+food=input("food: ")
+print("sweet") if food== "cake" or food== "jalebi" else print("not sweet")
+
+age =int(input("age:"))
+vote=("yes","no")[age<=18] #first value is for when the statement is false and secong for if it is true
+print(vote)
+
+sal=float(input("salary:"))
+tax=sal*(0.1,0.2)[sal<=5000]
+print(tax)
 
 a,b=5,2
 print("Arithmetic Operators")
---
+++ 04._Strings.py
@@ -0,0 +1,34 @@
+str1="this is a stirng\t"
+str2="arya"
+str3="\nmeow meow \nmeowww meow "
+print(str1+str2+str3)
+length=len(str1)
+print(len)  
+str="Arya"
+sliced=str[1:3]
+print(sliced)
+# sliced2=str[2:len(str)]
+
+str1="meowiamcute"
+sliced1=str1[2:len(str1)]
+print(sliced1)
+
+ca=str[:4]
+print(ca)
+str="apple"
+print(str[-3:-1])
+print(str[-5:-2])
+
+string1="when i Go go go  it goes"
+ends=string1.endswith("es")
+print(ends)
+capitalize=string1.capitalize()
+print(capitalize)
+replaced=string1.replace("i","m")
+print(replaced)
+f1=string1.find("boss")
+print(f1)
+f2=string1.find("Go")
+print(f2)
+cont=string1.count("go")
+print(cont)
