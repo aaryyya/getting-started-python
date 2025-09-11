b54bf45e77e5970388c332ec56d8dd1d716b57c7
aaryyya <aryampatil2005@gmail.com>
2025-09-11 08:35:49 +0530

    Questions Basic and Strings added

--- 10._Quetions.py
+++ 10._Quetions.py
@@ -1,8 +1,14 @@
-side=float(input("Enter Side here: "))
-areaofsq=side*side
-print(areaofsq)
+# side=float(input("Enter Side here: "))
+# areaofsq=side*side
+# print(areaofsq)
 
-num1=float(input("Enter Number_1 for calculating average here: "))
-num2=float(input("Enter Number_2 for calculating average here: "))
-average=(num1+num2)/2
-print(average)
+# num1=float(input("Enter Number_1 for calculating average here: "))
+# num2=float(input("Enter Number_2 for calculating average here: "))
+# average=(num1+num2)/2
+# print(average)
+
+
+num1=int(input("Enter number1 here: "))
+num2=int(input("Enter number2 here"))
+var1="True"if num1>=num2 else "False"
+print(var1)
