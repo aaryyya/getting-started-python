1b5ec0e216de3504323e727131ed830b46280f22
aaryyya <aryampatil2005@gmail.com>
2025-09-11 17:58:33 +0530

    Questions added

---
+++ 06._Tuples.py
@@ -0,0 +1,6 @@
+tup=(1,2,3,4,5,6,5,5,4,"dgh")
+print(tup)
+print(tup[0])
+print(tup[5])
+print(tup.index(3))
+print(tup.count(5))
--- 10._Quetions.py
+++ 10._Quetions.py
@@ -9,6 +9,20 @@
 
 
 num1=int(input("Enter number1 here: "))
-num2=int(input("Enter number2 here"))
-var1="True"if num1>=num2 else "False"
-print(var1)
+num2=int(input("Enter number2 here: "))
+num3=int(input("Enter number3 here: "))
+# var1="True"if num1>=num2 else "False"
+# print(var1)
+# print("Check Odd or Even")
+# if(num1%2==0):
+#     print("even")
+# else:
+#     print("odd")
+
+print("Check which is greater n1,n2 or n3")
+if(num1>=num2 and num1>=num3):
+    print("num1 is greater than equal to num2 n num3")
+elif(num2>=num3):
+    print("num2 is greater than equal to num3")
+else:
+    print("num3 is greater ")
