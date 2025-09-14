a3907ca6d062344d0c243752fb26217c2c2652b2
aaryyya <aryampatil2005@gmail.com>
2025-09-14 22:41:13 +0530

    oop1

---
+++ 13._OOP.py
@@ -0,0 +1,44 @@
+class Student:
+    college="ameowining"
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks
+
+    def welcome(self):
+        print("welcome stud,",self.name)
+
+    def get_marks(self):
+        return self.marks
+
+s1= Student("Siya",19)
+s1.welcome()
+print(s1.get_marks())
+
+
+
+#     fullname="queen"
+#     def __init__(self,fullname):
+#         # print(self)
+#         # print("Adding Stud")
+#     # name="karn"
+#         self.name=fullname
+
+# # s1=Student("crinen")
+# s1=Student("Arya")
+# print(Student.fullname)
+# # s2=Student("krik")
+# # print()
+# s2=Student("krik")
+# print(s2.college,s2.name)
+# print(Student.college,s2.name)
+
+# # s2=Student()
+# print(s2.name)
+
+# class Car:
+#     color="blue"
+#     brand="mercedes"
+
+# car1=Car()
+# print(car1.color)
+# print(car1.brand)
