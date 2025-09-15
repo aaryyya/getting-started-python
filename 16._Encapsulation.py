d6c927045f89a28591f76c743a03ba95580eafc7
aaryyya <aryampatil2005@gmail.com>
2025-09-15 20:38:17 +0530

    All type Inheritance done

--- 16._Encapsulation.py
+++ 16._Encapsulation.py
@@ -1,8 +1,23 @@
 class Account:
     def __init__(self,acc_no,acc_pass):
         self.acc_no=acc_no
-        self.acc_pass=acc_pass
+        self.__acc_pass=acc_pass
 
+    def resetpass(self):
+        print(self.__acc_pass)
 acc=Account("123","arya123")
 print(acc.acc_no)
-print(acc.acc_pass)
+# print(acc.__acc_pass)
+print(acc.resetpass())
+
+
+class Person:
+    __name="anonymous"
+    def __hello(self):
+        print("hello")
+
+    def welcome(self):
+        self.__hello()
+
+p1=Person()
+print(p1.welcome())
---
+++ 17._Inheritance.py
@@ -0,0 +1,44 @@
+# Single Inheritance
+
+class Car:
+    color="green"
+    @staticmethod
+    def start():
+        print("car started")
+
+    @staticmethod
+    def stop():
+        print("car stopped")
+
+class ToyotaCar(Car):
+    def __init__(self,name,brand):
+        self.name=name
+        self.brand=brand
+car1=ToyotaCar("fortuner","toyota")
+# car2=ToyotaCar("priss")
+
+print(car1.name)
+print(car1.start())
+print(car1.color)
+
+#Multilevel INheritance
+class Fortuner(ToyotaCar):
+    def __init__(self,type):
+        self.type=type
+
+car3=Fortuner("disel")
+car3.start()
+
+
+#MUltiple Inheritance
+class A:
+    varA="Welcome class A here"
+class B:
+    varB="Welcome class B here"
+class C(A,B):
+    varC="Welcome class C here"
+
+c1=C()
+print(c1.varC)
+print(c1.varB)
+print(c1.varA)
